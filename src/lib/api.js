aea0024f5d0a66ac43b1e009852695f8399fbd03
khaeru99 <khaerul.r.23@gmail.com>
2026-04-08 20:22:38 +0700

    update cors

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,11 +1,9 @@
 import axios from 'axios'
 import { setupInterceptors } from './api-interceptor'
 
-const URL = import.meta.env.VITE_CAMERA_URL || '/api/camera'
-
 const CAMERA_URL = import.meta.env.DEV
     ? '/'
-    : URL
+    : '/api/camera'
 
 const ApiClient = axios.create({
     baseURL: CAMERA_URL,
